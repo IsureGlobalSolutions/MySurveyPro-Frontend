25233d2487553a36a2b81681e5e7e88e8a537d67
saqibanayat <saqibanayat90@gmail.com>
2025-03-11 12:11:54 +0500

    upate

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "build",
+    chunkSizeWarningLimit: 1000, // Adjusted chunk size limit warning
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor';
+            }
+            if (id.includes('redux')) {
+              return 'redux-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
-  plugins: [react(),svgr()],
-  assetsInclude: ['**/*.xlsx','**/*.csv'],
+  plugins: [react(), svgr()],
+  assetsInclude: ['**/*.xlsx', '**/*.csv'],
 });
